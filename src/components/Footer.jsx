d68579528b28b4e0102b3bed7d5e6187a6b9d98e
Julio Yanez <jcesar206@hotmail.com>
2025-07-03 11:55:51 -0600

    Se actualiza footer-component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-blue-800 text-white text-center p-4 mt-10 w-full">
             <p className="text-1 text-white font-bold hover:text-blue-300 transition-colors">&copy; 2025. JulyDevops. Todos los derechos reservados.</p>
             <div className="flex justify-center gap-4 text-2xl">
-                <a href="www.linkedin.com/in/jcesar206" target="_blank" rel="noreferrer" className="hover:text-cyan-300 transition-colors">
+                <a href="https://www.linkedin.com/in/jcesar206" target="_blank" rel="noreferrer" className="hover:text-cyan-300 transition-colors">
                     <FaLinkedin />
                 </a>
                 <a href="https://github.com/JCesar206" target="_blank" rel="noreferrer" className="hover:text-rose-300 transition-colors">
