beb405f984f850403fe927bdb7008c5814f94f9c
JCesar206 <jcesar206@hotmail.com>
2025-09-29 18:55:46 -0600

    Se sube link e información de proyecto NotaLoca al igual que su imagen en carpeta public

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,11 @@ function App() {
     es: {
       title: "Mi Blog Personal", home: "Bienvenido a mi Blog Personal",
       desc: "Aquí comparto mis proyectos y experiencia en desarrollo web.", about: "Sobre mí",
-      aboutText: "Desarrollador web con experiencia en la creación de sitios y aplicaciones optimizadas para rendimiento y accesibilidad. Especializado en HTML, CSS, JavaScript y frameworks como React, Vue y TypeScript, con conocimientos en UX/UI y diseño responsivo. Busco una oportunidad en una empresa que valore la innovación y el desarrollo de soluciones digitales eficientes.", projects: "Proyectos", passManag: "Administrador de Contraseñas", ecommerce: "Ecommerce Loco", passGene: "Generador de contraseñas", contact: "Contacto", contactText: "Puedes contactarme en los siguientes medios:", contactText1: "¿Te interesa colaborar o tienes alguna duda? ¡Escribeme a", copyRight: "Todos los derechos reservados",
-      sendEmail: "Enviame un correo", seeProject: "Ver proyecto", madeWith: "Hecho con:",  
-      descPro1: "Aplicación administradora de contraseñas, el usuario puede colocar el sitio, usuario y contraseña y la app guarda localmente su información de forma segura.", descPro2: "Comercio electronico que permite sumar articulos al carrito, suma la cantidad total y descuenta los articulos del stock o los devuelve segun sea el caso. Simula una compra completa.", titlePro3: "", descPro3: "", titlePro4: "Finanzas Personales", descPro4: "Finanzas personales Aplicacion de finanzas personales hecha con Vue + Tailwind permite gestionar de forma simple los gastos e ingresos a una cuenta.", titlePro5: "Mini Red Social", descPro5: "Con esta app puedes subir imagenes locales, comentarios y colocarles emojis simples al igual que editar y eliminar el comentario. Disfruta esta mini red social.",
+      aboutText: "Desarrollador web con experiencia en la creación de sitios y aplicaciones optimizadas para rendimiento y accesibilidad. Especializado en HTML, CSS, JavaScript y frameworks como React, Vue y TypeScript, con conocimientos en UX/UI y diseño responsivo. Busco una oportunidad en una empresa que valore la innovación y el desarrollo de soluciones digitales eficientes.", projects: "Proyectos", passManag: "Administrador de Contraseñas", contact: "Contacto", contactText: "Puedes contactarme en los siguientes medios:", contactText1: "¿Te interesa colaborar o tienes alguna duda? ¡Escribeme a", copyRight: "Todos los derechos reservados", sendEmail: "Enviame un correo", seeProject: "Ver proyecto",madeWith: "Hecho con:", titlePro1: "Ecommerce Loco", descPro1: "Aplicación administradora de contraseñas, el usuario puede colocar el sitio, usuario y contraseña y la app guarda localmente su información de forma segura.",titlePro2: "Generador de contraseñas", descPro2: "Comercio electronico que permite sumar articulos al carrito, suma la cantidad total y descuenta los articulos del stock o los devuelve segun sea el caso. Simula una compra completa.", titlePro3: "Nota Loca", descPro3: "Nota Loca con Base de Datos. Este proyecto te permite tener una cuenta personal y poder crear notas, guardarlas, editarlas, eliminarlas de la base de datos.", titlePro4: "Finanzas Personales", descPro4: "Finanzas personales Aplicacion de finanzas personales hecha con Vue + Tailwind permite gestionar de forma simple los gastos e ingresos a una cuenta.", titlePro5: "Mini Red Social", descPro5: "Con esta app puedes subir imagenes locales, comentarios y colocarles emojis simples al igual que editar y eliminar el comentario. Disfruta esta mini red social.",
       tecnologies: "Conocimiento y uso de las siguientes tecnologias:", goToPage: "Ir a la página", home1: "Bienvenido",
     },
     en: {
-      title: "My Personal Blog", home: "Welcome to my Personal Blog", desc: "Here I share my projects and web development experience.",about: "About Me", aboutText: "Web developer with experience creating websites and applications optimized for performance and accessibility. Specialized in HTML, CSS, JavaScript, and frameworks such as React, Vue, and TypeScript, with knowledge of UX/UI and responsive design. I am looking for an opportunity at a company that values ​​innovation and the development of efficient digital solutions.", projects: "Projects", passManag: "Password Manager", ecommerce: "Crazy Ecommerce", passGene: "Password Generator", contact: "Contact", contactText: "You can reach me through the following channels:",contactText1: "Are you interested in collaborating or have any questions? Write to me at", copyRight: "All rights reserved", sendEmail: "Send me an email", madeWith: "Made with:", titlePro1: "Password Manager", descPro1: "Password manager application, the user can enter the site, username and password and the app saves their information locally and securely.", titlePro2: "Crazy Ecommerce", descPro2: "E-commerce that allows you to add items to your cart, add up the total amount, and deduct items from stock or return them as appropriate. Simulates a complete purchase.", titlePro3: "", descPro3: "", titlePro4: "Personal Finance", descPro4: "Personal Finance.A personal finance app built with Vue + Tailwind that allows you to easily manage expenses and income in an account.", titlePro5: "Mini Social Media", descPro5: "With this app, you can upload local images and comments, add simple emojis, and edit and delete comments. Enjoy this mini social network.", tecnologies: "Knowledge and use of the following technologies:", goToPage: "Go to page", home1: "Welcome",
+      title: "My Personal Blog", home: "Welcome to my Personal Blog", desc: "Here I share my projects and web development experience.",about: "About Me", aboutText: "Web developer with experience creating websites and applications optimized for performance and accessibility. Specialized in HTML, CSS, JavaScript, and frameworks such as React, Vue, and TypeScript, with knowledge of UX/UI and responsive design. I am looking for an opportunity at a company that values ​​innovation and the development of efficient digital solutions.", projects: "Projects", contact: "Contact", contactText: "You can reach me through the following channels:",contactText1: "Are you interested in collaborating or have any questions? Write to me at", copyRight: "All rights reserved", sendEmail: "Send me an email", madeWith: "Made with:", titlePro1: "Password Manager", descPro1: "Password manager application, the user can enter the site, username and password and the app saves their information locally and securely.", titlePro2: "Crazy Ecommerce", descPro2: "E-commerce that allows you to add items to your cart, add up the total amount, and deduct items from stock or return them as appropriate. Simulates a complete purchase.", titlePro3: "Crazy Note", descPro3: "Crazy Notes with Database. This project allows you to have a personal account and create notes, save them, edit them, and delete them from the database.", titlePro4: "Personal Finance", descPro4: "Personal Finance.A personal finance app built with Vue + Tailwind that allows you to easily manage expenses and income in an account.", titlePro5: "Mini Social Media", descPro5: "With this app, you can upload local images and comments, add simple emojis, and edit and delete comments. Enjoy this mini social network.", tecnologies: "Knowledge and use of the following technologies:", goToPage: "Go to page", home1: "Welcome",
     }
   };
 
@@ -66,7 +64,7 @@ function App() {
       <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-3 p-4">
 {/* Proyecto 1 */}
   <div class="relative max-w-sm rounded-lg overflow-hidden bg-white shadow-md shadow-purple-800 hover:shadow-xl transform transition duration-300 hover:scale-105">
-    <img class="w-full h-48 object-cover" src="./passwordManager01.jpg" alt="Password Manager" />
+    <img class="w-full h-48 object-cover" src="./passwordManager01.jpg" alt="Administrador de Contraseñas" />
     
     <div class="px-6 py-4 hover:shadow-purple-500 shadow-2xl">
       <div class="font-bold text-indigo-900 text-xl mb-2">{texts[language].titlePro1}</div>
@@ -109,30 +107,30 @@ function App() {
     </a>
   </div>
 {/* Proyecto 3 */}
-  <div class="relative max-w-sm rounded-lg overflow-hidden bg-white shadow-md shadow-purple-800 hover:shadow-xl transform transition duration-300 hover:scale-105">
-    <img class="w-full h-48 object-cover" src="./next.jpg" alt="Próximamente" />
+  <div class="relative max-w-sm rounded-lg overflow-hidden bg-white text-orange-600 shadow-md shadow-yellow-500 hover:shadow-xl transform transition duration-300 hover:scale-105">
+    <img class="w-full h-48 object-cover" src="./crazyNote01.jpg" alt="Nota Loca" />
     
     <div class="px-6 py-4">
-      <div class="font-bold text-indigo-900 text-xl mb-2">{texts[language].titlePro3}</div>
-      <p class="text-indigo-800 font-semibold text-base mb-4">
+      <div class="font-bold text-yellow-600 text-xl mb-2">{texts[language].titlePro3}</div>
+      <p class="text-yellow-600 font-semibold text-base mb-4">
         {texts[language].descPro3}
       </p>
       
-      <div class="flex flex-wrap gap-2 mt-4 mb-16 justify-center style-icon-card">
+      <div class="flex flex-wrap gap-2 mt-4 mb-16 justify-center style-icon-cardGold">
         <FaReact size={18} title="React"/>
         <SiTailwindcss size={18} title="Tailwind CSS"/>
       </div>
     </div>
     
-    <a href="#" class="absolute bottom-2 left-1/2 -translate-x-1/2 w-[90%] flex justify-center">
-      <button class="style-btn-gotopage">
+    <a href="https://jcesar206.github.io/notesFront/" target="_blank" class="absolute bottom-2 left-1/2 -translate-x-1/2 w-[90%] flex justify-center">
+      <button class="style-btn-gotopageGold">
         {texts[language].goToPage}
       </button>
     </a>
   </div>
 {/* Proyecto 4 */}
   <div class="relative max-w-sm rounded-lg overflow-hidden bg-white shadow-md shadow-purple-800 hover:shadow-xl transform transition duration-300 hover:scale-105">
-    <img class="w-full h-48 object-cover" src="./personalFinance01.jpg" alt="Personal Finance" />
+    <img class="w-full h-48 object-cover" src="./personalFinance01.jpg" alt="Finanzas Personales" />
     
     <div class="px-6 py-4">
       <div class="font-bold text-indigo-900 text-xl mb-2">{texts[language].titlePro4}</div>
@@ -154,7 +152,7 @@ function App() {
   </div>
 {/* Proyecto 5 */}
   <div class="relative max-w-sm rounded-lg overflow-hidden bg-white shadow-md shadow-purple-800 hover:shadow-xl transform transition duration-300 hover:scale-105">
-    <img class="w-full h-48 object-cover" src="./miniSocialMedia01.jpg" alt="Próximamente" />
+    <img class="w-full h-48 object-cover" src="./miniSocialMedia01.jpg" alt="Mini Red Social" />
     
     <div class="px-6 py-4">
       <div class="font-bold text-indigo-900 text-xl mb-2">{texts[language].titlePro5}</div>
