383eaaf27ee23cb83c39f800b7812c38c5cbe144
JCesar206 <jcesar206@hotmail.com>
2025-10-15 08:11:54 -0600

    Se actualiza app principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import {DiHtml5, DiCss3, DiJavascript1, DiMysql, DiReact, DiGit, DiGithub, DiNodejsSmall} from "react-icons/di";
 import {SiTailwindcss, SiPostman, SiTypescript, SiJest } from "react-icons/si";
 import {VscVscodeInsiders} from "react-icons/vsc";
-import { FaReact, FaVuejs } from "react-icons/fa";
+import { FaReact, FaVuejs, FaWhatsapp } from "react-icons/fa";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -27,7 +27,10 @@ function App() {
     <div className={darkMode ? "dark bg-gray-950 text-white" : "bg-white text-black"}>
       <Navbar language={language} setLanguage={setLanguage} darkMode={darkMode} setDarkMode={setDarkMode} />
 
-<main className="container mx-auto p-6 lg:p-16"> {/* About me */}
+<main className="container mx-auto p-6 lg:p-16"> 
+  {/* Botón flotante Whatsapp */}
+    <a href="https://wa.me/5217221828896" target="_blank" className="fixed bottom-20 right-4 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600"><FaWhatsapp size={24} /></a>
+  {/* About me */}
   <section id="home" className="items-center text-center font-extrabold text-4xl text-blue-950 dark:text-white hover:text-purple-400 pb-6">{texts[language].home1} 😎</section>
   <section id="about" className="min-h-[80vh] flex flex-col items-center justify-center text-center pb-12 px-4 py-3">
     <h1 className="style-header pb-12">😊 {texts[language].about}</h1>
