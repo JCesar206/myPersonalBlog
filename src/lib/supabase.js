e48e07fc5b22f27e27a34fc4e9455582a5a0ec30
JCesar206 <jcesar206@hotmail.com>
2025-12-30 17:41:50 -0600

    Se actualiza archivo supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -6,5 +6,6 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseKey) {
 	throw new Error("Supabase env vars are missing.");
 }
+console.log(import.meta.env);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
