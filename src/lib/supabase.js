2fea1150b44a0a3ad3275da5909b6d8e1a5161a4
JCesar206 <jcesar206@hotmail.com>
2025-12-30 18:12:07 -0600

    Se modifica archivo supabase.js y ci-cd.yml para despliegue en Github Pages

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,7 +4,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-	throw new Error("Supabase env vars are missing.");
+	console.warn("Supabase env vars are missing.");
 }
 console.log(import.meta.env);
 
