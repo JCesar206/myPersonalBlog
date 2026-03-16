317f8c305f667264f8614d8967c3c22eeb218373
Julio Cesar <jcesar206@hotmail.com>
2026-03-16 13:37:08 -0600

    Se modifica componente Card para manejo de textos, imagenes, links de las aplicaciones dentro del portafolio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,50 +1,45 @@
 import { useState } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
+import { texts } from "../i18n/texts.js"
 
-export default function Card({ title, description, image, link, language }) {
+export default function Card({ title, description, image, github, language }) {
+
   const [expanded, setExpanded] = useState(false);
   const maxLength = 70;
-  const shortText = description[language].slice(0, maxLength) + "...";
+
+  const text = description?.[language] || "";
+  const shortText =
+    description.length > maxLength ? description.slice(0, maxLength) + "..." : description;
 
   return (
     <article className="group relative bg-white dark:bg-gray-900 rounded-3xl overflow-hidden shadow-md hover:shadow-2xl transition-all duration-300">
-      {/*Image*/}
+
+      {/* Image */}
       <div className="relative overflow-hidden">
-        <img 
-          src={image}
-          alt={title[language]}
-          className="h-44 w-full object-cover transition-transform duration-500 group-hover:scale-110"
-        />
+        <img src={image} alt={title}
+          className="h-44 w-full object-cover transition-transform duration-500 group-hover:scale-110"/>
       </div>
 
-      {/*Arrow CTA*/}
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="absolute bottom-4 right-4 flex items-center justify-center w-10 h-10 rounded-full bg-indigo-600 text-white shadow-lg hover:bg-indigo-950 transition"
-        aria-label="View project"
-      >
+      {/* Arrow */}
+      <a href={github} target="_blank" rel="noopener noreferrer"
+        className="absolute bottom-4 right-4 flex items-center justify-center w-10 h-10 rounded-full bg-indigo-600 text-white shadow-lg hover:bg-indigo-950 transition cursor-pointer">
         <FiArrowUpRight size={20}/>
       </a>
 
-      {/*Content*/}
-        <div className="p-4 space-y-2">
-          <h3 className="text-base font-extrabold text-gray-900 dark:text-white">
-            {title[language]}
-          </h3>
+      {/* Content */}
+      <div className="p-4 space-y-2">
+        <h3 className="text-base font-extrabold text-gray-900 dark:text-white">
+          {title}
+        </h3>
+        <p className="text-sm text-gray-600 dark:text-gray-300 leading-snug">
+          {expanded ? description : shortText}
+        </p>
 
-          <p className="text-sm text-gray-600 dark:text-gray-300 
-          leading-snug">
-            {expanded ? description[language] : shortText}
-          </p>
-
-          <button
-            onClick={() => setExpanded(!expanded)}
-            className="text-3xl font-bold text-indigo-600 hover:underline hover:font-extrabold hover:text-indigo-950 cursor-pointer">
-            {expanded ? "-" : "+"}
-          </button>
-        </div>
+        <button onClick={() => setExpanded(!expanded)}
+          className="text-2xl font-bold text-indigo-600 hover:text-indigo-950 cursor-pointer">
+          {expanded ? "-" : "+"}
+        </button>
+      </div>
     </article>
   );
 }
