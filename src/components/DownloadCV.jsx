c148cbe2808da74ce52e6a7707d4c1857b874176
Julio Cesar <jcesar206@hotmail.com>
2026-03-16 13:42:28 -0600

    Se modifica componente DownloadDV para manejo de cambio de idioma centralizado

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,27 +1,16 @@
+import { texts } from "../i18n/texts.js"
 import { FiDownload } from "react-icons/fi";
-const texts = {
-	es: {
-		download: "Descargar CV",
-	},
-	en: {
-		download: "Download CV",
-	},
-};
 
 export default function DownloadCV({ compact = false, language = "es" }) {
 	const t = texts[language];
 
 	return (
-		<a
-			href={`${import.meta.env.BASE_URL}cv_Fullstack_JulioCYañezM.pdf`}
-			download
-			className={`inline-flex items-center gap-2 font-bold transition
-			${
-				compact ? "text-indigo-600 hover:text-indigo-950" :
-				"rounded-full bg-indigo-600 text-white px-6 py-3 shadow-lg hover:bg-indigo-950 cursor-pointer"
-			}`}
-			>
+		<a href={`${import.meta.env.BASE_URL}cv_Fullstack_JulioCYañezM.pdf`}
+			download className={`inline-flex items-center gap-2 font-bold transition
+			${compact ? "text-indigo-600 hover:text-indigo-950" :
+				"rounded-full bg-indigo-600 text-white text-xs px-6 py-3 shadow-lg hover:bg-indigo-950 cursor-pointer"}`}>
 				<FiDownload size={18}/>
+				{t.buttons.downloadCV}
 				{!compact && t.download}
 			</a>
 	);
