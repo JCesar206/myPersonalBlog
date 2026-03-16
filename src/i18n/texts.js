1747a9e3b11a0ba3d6e91c816181b52d3b192ea9
Julio Cesar <jcesar206@hotmail.com>
2026-03-16 13:33:41 -0600

    Se sube archivo texts como diccionario de la app

---
+++ src/i18n/texts.js
@@ -0,0 +1,147 @@
+export const texts = {
+  es: {
+    navbar: {
+      hero: "Acerca de mí",
+      technologies: "Tecnologías",
+      projects: "Proyectos",
+      contact: "Contacto"
+    },
+    hero: {
+      title: "Acerca de mí",
+      description: [
+        "Desarrollador web con experiencia en la creación de sitios y aplicaciones optimizadas para rendimiento y accesibilidad. Especializado en ","HTML", ", ", "CSS", ", ", "JavaScript",
+        " y frameworks como ","React", ", ", "Vue", ", ", "TypeScript"," y ", "Java",", con conocimientos en ",
+        "UX/UI"," y diseño responsivo."
+      ]
+    },
+    technologies: {
+      title: "Tecnologías y Herramientas"
+    },
+    contact: {
+      title: "Contacto",
+      question: "¿Tienes un proyecto o una idea? Escríbeme.",
+      legend: "Deja un comentario",
+      yourName: "Tu nombre",
+      yourEmail: "Tu correo (Opcional)",
+      writeYourComment: "Escribe tu comentario",
+      sending: "Enviando...",
+      error: "Ocurrió un error. Intenta de nuevo.",
+      sentComment: "Comentario enviado ✔",
+      clearFields: "Limpiar campos",
+      sendComment: "Enviar comentario"
+    },
+    buttons: {
+      downloadCV: "Descargar CV",
+      downloadCertificate: "Descargar certificado",
+      clearFields: "Limpiar campos",
+      sendComment: "Enviar comentario",
+      viewProject: "Ver proyecto",
+      viewCode: "Ver código"
+    },
+    projects: {
+      title: "Proyectos",
+      items: [
+        {
+          title: "Administrador de Contraseñas",
+          description: "Aplicación administradora de contraseñas. La app guarda localmente su nombre de usuario y contraseña de forma segura."
+        },
+				{
+          title: "Ecommerce",
+          description: "Comercio electrónico. Simula una compra completa de productos que se encontraban en un carrito de compras."
+        },
+				{
+          title: "Nota Loka",
+          description: "Nota Loca Local. La aplicación permite guardar notas en almacenamiento local."
+        },
+				{
+          title: "Finanzas Personales",
+          description: "Finanzas personales. Permite gestionar de forma simple los gastos e ingresos a una cuenta."
+        },
+				{
+          title: "Mini Red Social",
+          description: "Finanzas personales. Permite gestionar de forma simple los gastos e ingresos a una cuenta."
+        },
+				{
+          title: "PockétApi App",
+          description: "PocketApi App Esta app permite buscar a tu pokémon favorito por nombre o número de identificador mostrando sus caracteristicas y hábilidades. Un pequeño viaje a la nostalgia."
+        }
+      ]
+    },
+		footer: {
+			nameApp: "Mi portafolio personal.",
+			rights: "Todos los derechos reservados."
+		}
+  },
+  en: {
+    navbar: {
+      hero: "About",
+      technologies: "Technologies",
+      projects: "Projects",
+      contact: "Contact"
+    },
+    hero: {
+      title: "About me",
+      description: [
+        "Web developer with experience creating websites and applications optimized for performance and accessibility. Specialized in ","HTML", ", ", "CSS", ", ", "JavaScript"," and frameworks such as ",
+        "React", ", ", "Vue", ", ", "TypeScript"," and ", "Java",", with knowledge of ","UX/UI",
+        " and responsive design."
+      ]
+    },
+    technologies: {
+      title: "Technologies and Tools"
+    },
+    contact: {
+      title: "Contact",
+      question: "Have a project or an idea? Let's talk.",
+      legend: "Leave a comment",
+      yourName: "Your name",
+      yourEmail: "Your email (Optional)",
+      writeYourComment: "Write your comment",
+      sending: "Sending...",
+      error: "Something went wrong. Try again.",
+      sentComment: "Comment sent ✔",
+      clearFields: "Clear fields",
+      sendComment: "Send comment"
+    },
+    buttons: {
+      downloadCV: "Download CV",
+      downloadCertificate: "Download certificate",
+      clearFields: "Clear fields",
+      sendComment: "Send comment",
+      sending: "Sending..."
+    },
+    projects: {
+      title: "Projects",
+      items: [
+        {
+          title: "Password Manager",
+          description: "Password manager app. The app securely stores your username and password locally."
+        },
+        {
+          title: "Crazy Ecommerce",
+          description: "E-commerce. Simulates a complete purchase of products that were in a shopping cart."
+        },
+				{
+          title: "Crazy Note",
+          description: "Note Loca Local. The application allows you to save notes in localStorage."
+        },
+				{
+          title: "Personal Finance",
+          description: "Personal finance. It allows you to easily manage expenses and income to an account."
+        },
+				{
+          title: "Mini Social Media",
+          description: "A mini social network that allows you to add images, emojis, and comments in a fun way."
+        },
+				{
+          title: "PocketApi App",
+          description: "PocketApi App This app lets you search for your favorite Pokémon by name or ID number, displaying its characteristics and abilities. A little trip down memory lane."
+        }
+      ]
+    },
+		footer: {
+			nameApp: "My personal portfolio.",
+			rights: "All rights reserved."
+		}
+  }
+};
