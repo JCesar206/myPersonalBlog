f0f061d69bd349013cf950e0e4e880a257d858d2
Julio Cesar <jcesar206@hotmail.com>
2026-03-20 05:54:14 -0600

    Se actualiza diseño del componente

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -6,9 +6,12 @@ export default function DownloadCV({ compact = false, language = "es" }) {
 
 	return (
 		<a href={`${import.meta.env.BASE_URL}cv_Fullstack_JulioCYañezM.pdf`}
-			download className={`inline-flex items-center gap-2 font-bold transition
-			${compact ? "text-indigo-600 hover:text-indigo-950" :
-				"rounded-full bg-indigo-600 text-white text-xs px-6 py-3 shadow-lg hover:bg-indigo-950 cursor-pointer"}`}>
+			download
+			className={`inline-flex items-center gap-2 font-bold transition
+			${compact ? 
+				"text-indigo-600 hover:text-indigo-950" 
+				: `rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white 
+				text-xs px-6 py-3 shadow-lg hover:from-indigo-700 hover:to-pink-700 cursor-pointer hover:scale-105 transition-transform`}`}>
 				<FiDownload size={18}/>
 				{t.buttons.downloadCV}
 				{!compact && t.download}
