234342934dc7131577dc899dcc6c12a61046629b
Julio Cesar <jcesar206@hotmail.com>
2026-03-20 05:51:39 -0600

    Se actualiza diseño del componente

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ export default function Projects({ language }) {
 
   return (
     <section id="projects" className="space-y-10">
-      <h2 className="text-2xl md:text-3xl font-extrabold text-center text-gray-900 dark:text-indigo-700">
+      <h2 className="text-center text-4xl md:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient-x hover:scale-105">
         {t.projects.title}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
