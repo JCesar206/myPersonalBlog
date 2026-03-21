a47ec3487b069ba16b36ca298d4e93fb19b8c554
Julio Cesar <jcesar206@hotmail.com>
2026-03-21 06:30:17 -0600

    Se actualiza diseño del componente

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { TbMessageLanguage } from "react-icons/tb";
 import { FaSun, FaMoon } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 import { MdOutlineSegment } from "react-icons/md";
-import DownloadCV from "./DownloadCV";
-import DownloadCertificate from "./DownloadCertificate";
 import { texts } from "../i18n/texts.js";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import DownloadCV from "./DownloadCV";
+import DownloadCertificate from "./DownloadCertificate";
 
 export default function Navbar({ setDarkMode, setLanguage, language, darkMode }) {
   const t = texts[language];
@@ -68,7 +68,7 @@ export default function Navbar({ setDarkMode, setLanguage, language, darkMode })
             ))}
           </div>
 
-          {/* Links SocialMedia */}
+          {/* Links SocialMedia Desktop */}
           <div className="hidden md:flex items-center gap-3 font-serif">
             <a href="https://github.com/JCesar206" target="_blank" rel="noopener noreferrer"
             className="hover:scale-145 hover:text-purple-600 dark:hover:text-violet-400">
@@ -82,31 +82,31 @@ export default function Navbar({ setDarkMode, setLanguage, language, darkMode })
 
           {/* Controls */}
           <div className="flex items-center gap-4">
-            {/* Social Media para pantallas chicas */}
+            {/* Links SocialMedia Desktop Mobile */}
             <div className="flex md:hidden items-center gap-3">
               <a href="https://github.com/JCesar206" target="_blank" rel="noopener noreferrer"
-                className="text-gray-700 dark:text-gray-200 hover:text-purple-600 dark:hover:text-violet-400 transition hover:scale-110"><FaGithub size={20}/>
+                className="text-gray-700 dark:text-gray-200 hover:text-purple-600 dark:hover:text-violet-400 transition hover:scale-150"><FaGithub size={20}/>
               </a>
               <a href="https://www.linkedin.com/in/jcesar206" target="_blank" rel="noopener noreferrer"
-                className="text-gray-700 dark:text-gray-200 hover:text-purple-600 dark:hover:text-violet-400 transition hover:scale-110"><FaLinkedin size={20} />
+                className="text-gray-700 dark:text-gray-200 hover:text-purple-600 dark:hover:text-violet-400 transition hover:scale-150"><FaLinkedin size={20} />
             </a>
             </div>
             {/* Theme */}
             <button onClick={toggleTheme}
-              className="hover:scake-150 hover:text-purple-600 dark:hover:text-violet-400 transition cursor-pointer">
-              {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+              className="hover:text-purple-600 dark:hover:text-violet-400 transition cursor-pointer hover:scale-150">
+              {darkMode ? <FaSun size={20}/> : <FaMoon size={20}/>}
             </button>
 
             {/* Language */}
             <button onClick={toggleLanguage}
-              className="text-gray-700 dark:text-gray-200 hover:text-indigo-600 transition cursor-pointer">
-              <TbMessageLanguage size={20} />
+              className="hover:text-purple-600 dark:hover:text-violet-400 transition cursor-pointer hover:scale-150">
+              <TbMessageLanguage size={20}/>
             </button>
 
             {/* Mobile menu */}
             <button onClick={() => setOpen(!open)}
-              className="md:hidden text-gray-700 dark:text-gray-200 hover:text-indigo-600 transition cursor-pointer">
-              {open ? <IoClose size={20} /> : <MdOutlineSegment size={20} />}
+              className="md:hidden hover:text-purple-600 dark:hover:text-violet-400 transition cursor-pointer hover:scale-150">
+              {open ? <IoClose size={20}/> : <MdOutlineSegment size={20}/>}
             </button>
           </div>
         </div>
@@ -115,11 +115,10 @@ export default function Navbar({ setDarkMode, setLanguage, language, darkMode })
       {/* Mobile menu */}
       {open && (
         <div className="fixed inset-0 z-40 bg-black/40 backdrop-blur-sm md:hidden">
-          <div ref={menuRef} className="absolute top-24 left-1/2 -translate-x-1/2 w-[90%] bg-white dark:bg-gray-900 rounded-3xl shadow-2xl p-6 flex flex-col gap-6 text-center"
-          >
+          <div ref={menuRef} className="absolute top-24 left-1/2 -translate-x-1/2 w-[90%] bg-white dark:bg-gray-900 rounded-3xl shadow-2xl p-6 flex flex-col gap-6 text-center">
             {navItems.map((item) => (
               <a key={item.id} href={`#${item.id}`} onClick={() => setOpen(false)}
-                className="font-semibold hover:text-indigo-600 hover:font-extrabold transition">
+                className="font-semibold dark:hover:text-violet-400 hover:font-extrabold transition">
                 {item.label}
               </a>
             ))}
