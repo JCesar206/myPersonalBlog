c777fb45e537f7300e6b7b36c232d290bfb90fc2
Julio Cesar <jcesar206@hotmail.com>
2026-03-28 10:53:15 -0600

    Se modifica diseño del componente CurrentJob

--- src/components/CurrentJob.jsx
+++ src/components/CurrentJob.jsx
@@ -6,7 +6,7 @@ export default function CurrentJob ({ language = "es" }) {
 	const contact = texts[language].contact;
 
 	return (
-		<div className="flex items-center space-x-4 p-6 bg-gray-200/50 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-500 dark:bg-gray-900">
+		<div className="flex items-center space-x-4 p-6 bg-gray-200/50 rounded-3xl shadow-md hover:shadow-lg transition-shadow duration-500 dark:bg-gray-900">
 			<a href="https://www.asteroi.dev/" target="_blank" rel="noopener noreferrer">
 			<img src="/asteroidev.svg" alt="Asteroi.dev" className="bg-purple-900 w-12 h-12 rounded-full object-cover animate-pulse" title={contact.visit}/>
 			</a>
@@ -14,9 +14,9 @@ export default function CurrentJob ({ language = "es" }) {
 				<h2 className="text-xl font-bold text-gray-800 text-center dark:text-white">
 					{contact.namePosition}
 				</h2>
-				<p className="text-gray-600 font-semibold text-center">{contact.iHaveWork}</p>
+				<p className="text-gray-600 dark:text-gray-300 font-semibold text-center">{contact.iHaveWork}</p>
 				<a href="https://www.asteroi.dev/" target="_blank" rel="noopener noreferrer">
-				<p className="text-gray-700 font-bold text-center hover:underline">Asteroi.dev</p>
+				<p className="text-gray-700 dark:text-white font-bold text-center hover:underline">Asteroi.dev</p>
 				</a>
 			</div>
 		</div>
